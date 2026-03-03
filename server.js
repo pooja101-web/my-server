11989290c2aca0fef29cc05a6f40e5be1ee35d1d
Pooja <Poojapahade88@gmail.com>
2026-03-03 16:32:06 +0530

    addedfiles

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server is running successfully 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
